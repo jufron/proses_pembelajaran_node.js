027ea73fa871193215cae684c7b72fd19ddf98f8
jufron tamo ama <jufrontamoama@gmail.com>
2023-05-21 20:38:57 +0800

    memahami penggunaan report

---
+++ 5 report/index.js
@@ -0,0 +1,23 @@
+
+
+
+// process.report.reportOnFatalError = true;
+// process.report.reportOnUncaughtException = false;
+// process.report.reportOnSignal = true;
+
+
+// * simpan file 
+// process.report.filename = './report.json';
+// process.report.writeReport('./report.json');
+
+// function exampleError () {
+//   throw new Error('new error from user');
+// }
+
+// try {
+//   exampleError();
+  
+// } catch (err) {
+//   console.log(err);
+//   process.report.writeReport('./report.json');
+// }
